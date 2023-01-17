4100ff83b5a6a3bb8636c7b8edd5f97c5ce2325b
shongie <116272145+shongie@users.noreply.github.com>
2023-01-17 09:52:22 +0200

    fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from './Login';
+import Registration from './Register';
+import ForgotPasswordPage from './password'
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div >
+  
+      <Router>
+        <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='/forgot-password' element={<ForgotPasswordPage/>}></Route>
+        <Route path='/register' element={<Registration/>}></Route>
+         
+        </Routes>
+      </Router>
+
+    
     </div>
+
+   
+
   );
 }
 
