24e7d698e033289d455c916cbf9182cd4729f489
Mankopodi Kgopotso <kgopotsomaloma170@gmail.com>
2023-01-17 12:42:02 +0200

    pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Bookings from '../src/customers/bookings/bookings';
 
 function App() {
   return (
     <div className="App">
-     
+     <Bookings />
     </div>
   );
 }
