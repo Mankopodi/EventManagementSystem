0bd5a2ee32041467eee0050a7bd13d6263cc8fa3
shongie <116272145+shongie@users.noreply.github.com>
2023-01-18 14:12:42 +0200

    additing ratings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Auth/Login';
 import Registration from './Auth/Register';
 import ForgotPasswordPage from './Auth/forgot password'
+import InjectedCheckoutForm from "./customers/payment/payment";
+import ProductReview1 from "./eventPlanner/ratings&Ratings/rating"; 
+
 
 import "./App.css";
 
@@ -16,9 +19,16 @@ function App() {
         <Route path='/' element={<Login/>}></Route>
         <Route path='/forgot-password' element={<ForgotPasswordPage/>}></Route>
         <Route path='/register' element={<Registration/>}></Route>
-         
+        
+        <Route path='/payment' element={<InjectedCheckoutForm/>}></Route>
+        <Route path='/review' element={<ProductReview1/>}></Route>
+     
+       
         </Routes>
       </Router>
+
+
+    
 
     
     </div>
