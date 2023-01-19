e945086b284bae14a5b91c0dfaa38d18cf87ce00
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2023-01-19 08:12:39 +0200

    frontend only

--- backend/src/api/customize-events-package/controllers/customize-events-package.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * customize-events-package controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::customize-events-package.customize-events-package');
