bedd85a1ca5ad8ac2aed4c5adda8efef569d67ed
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2023-01-20 14:31:11 +0200

    notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 
 import Packages from './customers/packages/packages';
+import PushNotification from './customers/pushNotification/pushNotification'
 
 function App() {
   return (
     <div className="App">
       <Packages />
+      <PushNotification />
      
     </div>
   );
