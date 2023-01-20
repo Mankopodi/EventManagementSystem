e8d59afeec7acfe9449ee4bec9a84144ad85b0f3
shongie <116272145+shongie@users.noreply.github.com>
2023-01-20 14:57:30 +0200

    adding review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Registration from './Auth/Register';
 import ForgotPasswordPage from './Auth/forgot password'
 import InjectedCheckoutForm from "./customers/payment/payment";
 import ProductReview1 from "./eventPlanner/ratings&Ratings/rating"; 
-
+import CRUD from "./eventPlanner/ratings&Ratings/addReview";
 
 import "./App.css";
 
@@ -21,7 +21,7 @@ function App() {
         <Route path='/register' element={<Registration/>}></Route>
         <Route path='/payment' element={<InjectedCheckoutForm/>}></Route>
         <Route path='/review' element={<ProductReview1/>}></Route>
-     
+        <Route path='/addreview' element={<CRUD/>}></Route>
        
         </Routes>
       </Router>
