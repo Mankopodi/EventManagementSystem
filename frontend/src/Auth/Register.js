3b0e1080c5b8feca9c477feee2d7b305e02f9f95
Mankopodi Kgopotso <kgopotsomaloma170@gmail.com>
2023-01-27 08:46:11 +0200

    changes

--- frontend/src/Auth/Register.js
+++ frontend/src/Auth/Register.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+
 const config = require("./config.json");
 
  export default function Registration() {
